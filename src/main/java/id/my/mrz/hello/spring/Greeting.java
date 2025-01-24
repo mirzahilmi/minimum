23f2f6577b2ad258ccbd1495190dc95143bd7714
Mirza Hilmi <dev@mrz.my.id>
2025-01-24 20:24:09 +0700

    feat: basic restful api & init repo

---
+++ src/main/java/id/my/mrz/hello/spring/Greeting.java
@@ -0,0 +1,4 @@
+package id.my.mrz.hello.spring;
+
+public record Greeting(long id, String name) {
+}
---
+++ src/test/java/id/my/mrz/hello/spring/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.mrz.hello.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
