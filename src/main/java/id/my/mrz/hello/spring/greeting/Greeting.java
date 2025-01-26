4fb91f368c82b8f858c0161e384f00668c552035
Mirza Hilmi <dev@mrz.my.id>
2025-01-26 15:33:30 +0700

    refactor: move greeting resource into its domain folder

--- src/main/java/id/my/mrz/hello/spring/greeting/Greeting.java
+++ src/main/java/id/my/mrz/hello/spring/greeting/Greeting.java
@@ -1,4 +1,4 @@
-package id.my.mrz.hello.spring;
+package id.my.mrz.hello.spring.greeting;
 
 public record Greeting(long id, String name) {
 }
--- src/main/java/id/my/mrz/hello/spring/greeting/GreetingController.java
+++ src/main/java/id/my/mrz/hello/spring/greeting/GreetingController.java
@@ -1,4 +1,4 @@
-package id.my.mrz.hello.spring;
+package id.my.mrz.hello.spring.greeting;
 
 import java.util.concurrent.atomic.AtomicLong;
 
