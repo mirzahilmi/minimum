104e369ec1e64a63dd20520f2d38f84224c5939b
Mirza Hilmi <dev@mrz.my.id>
2025-01-31 05:48:13 +0700

    refactor: use type interface of passwordencoder instead

--- src/main/java/id/my/mrz/hello/spring/user/UserService.java
+++ src/main/java/id/my/mrz/hello/spring/user/UserService.java
@@ -3,16 +3,17 @@ package id.my.mrz.hello.spring.user;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements IUserService {
   private final IUserRepository repository;
-  // Should use DI?
-  private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+  private final PasswordEncoder encoder;
 
   public UserService(IUserRepository repository) {
     this.repository = repository;
+    this.encoder = new BCryptPasswordEncoder(12);
   }
 
   @Override
