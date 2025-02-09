04db9d223928c60535e73c130d3eee7aaab2db27
Mirza Hilmi <dev@mrz.my.id>
2025-02-09 07:44:52 +0700

    feat: pass violation tags for filtering in global error handler

--- src/main/java/id/my/mrz/hello/spring/GlobalExceptionHandler.java
+++ src/main/java/id/my/mrz/hello/spring/GlobalExceptionHandler.java
@@ -1,15 +1,25 @@
 package id.my.mrz.hello.spring;
 
+import id.my.mrz.hello.spring.exception.ResourceViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public final class GlobalExceptionHandler {
   @ExceptionHandler(UsernameNotFoundException.class)
   public ProblemDetail handleUsernameNotFoundException(UsernameNotFoundException e) {
     return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+  }
+
+  @ExceptionHandler(ResourceViolationException.class)
+  public ProblemDetail handleResourceViolationException(ResourceViolationException e) {
+    // HACK: maybe better off put this in constants
+    if (e.getTags().contains("duplicate"))
+      return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
+
+    return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
   }
 }
--- src/main/java/id/my/mrz/hello/spring/exception/ResourceViolationException.java
+++ src/main/java/id/my/mrz/hello/spring/exception/ResourceViolationException.java
@@ -1,11 +1,26 @@
 package id.my.mrz.hello.spring.exception;
 
+import java.util.List;
+
 public final class ResourceViolationException extends RuntimeException {
+  private final List<String> tags;
+
   public ResourceViolationException(String message) {
     super(message);
+    this.tags = List.of();
   }
 
   public ResourceViolationException(String message, Throwable cause) {
     super(message, cause);
+    this.tags = List.of();
+  }
+
+  public ResourceViolationException(String message, List<String> tags, Throwable cause) {
+    super(message, cause);
+    this.tags = tags;
+  }
+
+  public List<String> getTags() {
+    return tags;
   }
 }
