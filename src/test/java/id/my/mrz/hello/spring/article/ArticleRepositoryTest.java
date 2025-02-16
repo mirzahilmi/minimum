664a3d2b049a3b8ebc357124fc16dd119fae8b29
Mirza Hilmi <dev@mrz.my.id>
2025-02-16 11:01:56 +0700

    feat(article): get created data repository unit test

--- src/test/java/id/my/mrz/hello/spring/article/ArticleRepositoryTest.java
+++ src/test/java/id/my/mrz/hello/spring/article/ArticleRepositoryTest.java
@@ -3,7 +3,10 @@ package id.my.mrz.hello.spring.article;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import id.my.mrz.hello.spring.tag.Tag;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -21,18 +24,44 @@ class ArticleRepositoryTest {
   @Container @ServiceConnection
   static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>("postgres:17.3-alpine3.21");
 
-  @Autowired IArticleRepository articleRepository;
+  @Autowired IArticleRepository repository;
+
+  @BeforeEach
+  void setup() {
+    repository.deleteAll();
+  }
 
   @Test
-  void shouldReturnIterableContainSingleArticleWhenCreated() {
+  void fetchCreatedArticleContains() {
     Article article = new Article("title", "slug", "content", List.of(new Tag("tag")));
-    articleRepository.save(article);
-    Iterable<Article> articles = articleRepository.findAll();
+    create(article);
 
+    Iterable<Article> articles = repository.findAll();
     assertThat(articles)
         .singleElement()
         .usingRecursiveAssertion()
         .ignoringFields("id", "tags.id")
         .isEqualTo(article);
+  }
+
+  @Test
+  void getCreatedArticle() {
+    Article article = new Article("title", "slug", "content", List.of(new Tag("tag")));
+    Article created = create(article);
+
+    Optional<Article> _article = repository.findById(created.getId());
+
+    assertThat(_article.orElseThrow())
+        .usingRecursiveAssertion()
+        .ignoringFields("id", "tags.id")
+        .isEqualTo(article);
+  }
+
+  Article create(Article article) {
+    return repository.save(article);
+  }
+
+  Iterable<Article> create(Article... articles) {
+    return repository.saveAll(Arrays.asList(articles));
   }
 }
