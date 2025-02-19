7deaf2bee95e8ef85dd54c39cf2c289590940107
Mirza Hilmi <dev@mrz.my.id>
2025-02-19 14:41:06 +0700

    feat(article): update repository unit test to incude user

--- src/test/java/id/my/mrz/hello/spring/article/ArticleRepositoryTest.java
+++ src/test/java/id/my/mrz/hello/spring/article/ArticleRepositoryTest.java
@@ -3,6 +3,8 @@ package id.my.mrz.hello.spring.article;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import id.my.mrz.hello.spring.tag.Tag;
+import id.my.mrz.hello.spring.user.IUserRepository;
+import id.my.mrz.hello.spring.user.User;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -25,43 +27,53 @@ class ArticleRepositoryTest {
   @Container @ServiceConnection
   static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>("postgres:17.3-alpine3.21");
 
-  @Autowired IArticleRepository repository;
+  @Autowired private IArticleRepository repository;
+
+  @Autowired private IUserRepository userRepository;
+
+  private User testUser;
 
   @BeforeEach
   void setup() {
     repository.deleteAll();
+    userRepository.deleteAll();
+
+    testUser = new User("testuser", "password");
+    testUser = userRepository.save(testUser);
   }
 
   @Test
   void fetchCreatedArticleContains() {
-    Article article = new Article("title", "slug", "content", List.of(new Tag("tag")));
+    Article article = new Article("title", "slug", "content", testUser, List.of(new Tag("tag")));
     create(article);
 
     Iterable<Article> articles = repository.findAll();
+
     assertThat(articles)
         .singleElement()
         .usingRecursiveAssertion()
-        .ignoringFields("id", "tags.id")
+        .ignoringFields("id", "tags.id", "user.id")
         .isEqualTo(article);
   }
 
   @Test
   void createAndGetArticle() {
-    Article article = new Article("title", "slug", "content", List.of(new Tag("tag")));
+    Article article = new Article("title", "slug", "content", testUser, List.of(new Tag("tag")));
     Article created = create(article);
 
     Optional<Article> _article = repository.findById(created.getId());
 
     assertThat(_article.orElseThrow())
         .usingRecursiveAssertion()
-        .ignoringFields("id", "tags.id")
+        .ignoringFields("id", "tags.id", "user.id")
         .isEqualTo(article);
   }
 
   @Test
   void updateAndAddNewTagsToArticle() {
     Article article =
-        new Article("title", "slug", "content", new LinkedList<>(List.of(new Tag("tag"))));
+        new Article(
+            "title", "slug", "content", testUser, new LinkedList<>(List.of(new Tag("tag"))));
     Article created = create(article);
 
     article = repository.findById(created.getId()).orElseThrow();
@@ -74,22 +86,21 @@ class ArticleRepositoryTest {
 
     repository.save(article);
 
-    Article udpated = repository.findById(created.getId()).orElseThrow();
-    assertThat(udpated)
+    Article updated = repository.findById(created.getId()).orElseThrow();
+    assertThat(updated)
         .usingRecursiveAssertion()
-        .ignoringFields("id", "tags.id")
+        .ignoringFields("id", "tags.id", "user.id")
         .isEqualTo(article);
   }
 
   @Test
   void deleteArticle() {
-    Article article = new Article("title", "slug", "content", List.of(new Tag("tag")));
+    Article article = new Article("title", "slug", "content", testUser, List.of(new Tag("tag")));
     article = create(article);
 
     repository.deleteById(article.getId());
 
     Optional<Article> _article = repository.findById(article.getId());
-
     assertThat(_article).isEmpty();
   }
 
