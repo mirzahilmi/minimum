08e0f48dcea5971c1244a04d1c43a08c1e5035a1
Mirza Hilmi <dev@mrz.my.id>
2025-02-19 14:42:16 +0700

    feat(article): add redis testcontainers and implement integration test for get operation

--- src/test/java/id/my/mrz/hello/spring/article/ArticleIntegrationTest.java
+++ src/test/java/id/my/mrz/hello/spring/article/ArticleIntegrationTest.java
@@ -2,6 +2,11 @@ package id.my.mrz.hello.spring.article;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.redis.testcontainers.RedisContainer;
+import id.my.mrz.hello.spring.session.SessionCreateRequest;
+import id.my.mrz.hello.spring.session.SessionCreatedResponse;
+import id.my.mrz.hello.spring.user.IUserRepository;
+import id.my.mrz.hello.spring.user.UserSignupRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +15,17 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.client.MockMvcClientHttpRequestFactory;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.assertj.MockMvcTester.MockMvcRequestBuilder;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClient;
 import org.testcontainers.containers.MinIOContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -28,11 +40,13 @@ final class ArticleIntegrationTest {
   @Container @ServiceConnection
   static PostgreSQLContainer<?> postgresql = new PostgreSQLContainer<>("postgres:17.3-alpine3.21");
 
+  @Container @ServiceConnection
+  static RedisContainer redis = new RedisContainer("redis:7.4.2-alpine3.21");
+
   @Container
   static MinIOContainer minio = new MinIOContainer("minio/minio:RELEASE.2025-02-07T23-21-09Z");
 
   @Autowired MockMvc mockMvc;
-  RestClient client;
 
   @DynamicPropertySource
   static void minioProperties(DynamicPropertyRegistry registry) {
@@ -40,17 +54,93 @@ final class ArticleIntegrationTest {
     registry.add("minio.key.secret", minio::getPassword);
     registry.add("minio.endpoint", minio::getS3URL);
     registry.add("minio.bucket", () -> "bucket");
+    registry.add("spring.redis.host", redis::getHost);
+    registry.add("spring.redis.port", () -> redis.getRedisPort());
   }
 
   @BeforeEach
-  void setup() {
-    client =
-        RestClient.builder().requestFactory(new MockMvcClientHttpRequestFactory(mockMvc)).build();
+  void setup(
+      @Autowired IArticleRepository articleRepository, @Autowired IUserRepository userRepository) {
+    articleRepository.deleteAll();
+    userRepository.deleteAll();
   }
 
   @Test
   void getGreeting() {
+    RestClient client =
+        RestClient.builder().requestFactory(new MockMvcClientHttpRequestFactory(mockMvc)).build();
     String result = client.get().uri("/greeting").retrieve().body(String.class);
     assertThat(result).contains("Hello");
+  }
+
+  @Test
+  void Get_articles_data_with_empty_datastore() {
+    RestClient client =
+        RestClient.builder().requestFactory(new MockMvcClientHttpRequestFactory(mockMvc)).build();
+
+    String accessToken = authenticate();
+    assertThat(accessToken).isNotNull().isNotBlank();
+
+    ResponseEntity<ArticleResourceResponse[]> response =
+        client
+            .get()
+            .uri("/api/v1/articles")
+            .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", accessToken))
+            .retrieve()
+            .toEntity(ArticleResourceResponse[].class);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(response.getBody()).isNotNull().hasSize(0).isEmpty();
+  }
+
+  @Test
+  void Get_single_article_data_when_datastore_empty() {
+    MockMvcTester tester = MockMvcTester.create(mockMvc);
+
+    String accessToken = authenticate();
+    assertThat(accessToken).isNotNull().isNotBlank();
+
+    MockMvcRequestBuilder result =
+        tester
+            .get()
+            .uri("/api/v1/articles/{id}", 1)
+            .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", accessToken))
+            .accept(MediaType.APPLICATION_JSON);
+    assertThat(result)
+        .hasStatus(HttpStatus.NOT_FOUND.value())
+        .hasContentType(MediaType.APPLICATION_PROBLEM_JSON);
+  }
+
+  String authenticate() {
+    RestClient client =
+        RestClient.builder().requestFactory(new MockMvcClientHttpRequestFactory(mockMvc)).build();
+
+    UserSignupRequest credential =
+        new UserSignupRequest(
+            "username", "admin123aksdhjfkajhsdfkahdjsf", "admin123aksdhjfkajhsdfkahdjsf");
+    try {
+      client
+          .post()
+          .uri("/api/v1/users")
+          .contentType(MediaType.APPLICATION_JSON)
+          .body(credential)
+          .retrieve()
+          .toBodilessEntity();
+    } catch (Exception e) {
+      assertThat(e).hasCauseInstanceOf(HttpClientErrorException.class).hasMessage("404");
+    }
+
+    SessionCreateRequest sessionRequest =
+        new SessionCreateRequest(credential.getUsername(), credential.getPassword());
+    ResponseEntity<SessionCreatedResponse> response =
+        client
+            .post()
+            .uri("/api/v1/users/self/sessions")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(sessionRequest)
+            .retrieve()
+            .toEntity(SessionCreatedResponse.class);
+
+    return response.getBody().accessToken();
   }
 }
