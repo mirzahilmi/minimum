9e09f5f723b40157dce07b51ffedd204464a8b0a
Mirza Hilmi <dev@mrz.my.id>
2025-02-20 13:43:17 +0700

    feat: implement serializable

--- src/main/java/id/my/mrz/hello/spring/tag/dto/TagCreateRequest.java
+++ src/main/java/id/my/mrz/hello/spring/tag/dto/TagCreateRequest.java
@@ -2,7 +2,8 @@ package id.my.mrz.hello.spring.tag.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.io.Serializable;
 
 public record TagCreateRequest(
-    @NotNull(message = "name is required") @NotBlank(message = "name cannot be blank")
-        String name) {}
+    @NotNull(message = "name is required") @NotBlank(message = "name cannot be blank") String name)
+    implements Serializable {}
