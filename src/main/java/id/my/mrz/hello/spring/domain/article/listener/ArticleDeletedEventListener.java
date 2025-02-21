e25c43c6111a8b3602180057c854adc56bdde7db
Mirza Hilmi <dev@mrz.my.id>
2025-02-21 13:29:59 +0700

    feat(article): delete index data upon article deleted event

--- src/main/java/id/my/mrz/hello/spring/domain/article/listener/ArticleDeletedEventListener.java
+++ src/main/java/id/my/mrz/hello/spring/domain/article/listener/ArticleDeletedEventListener.java
@@ -1,14 +1,21 @@
 package id.my.mrz.hello.spring.domain.article.listener;
 
 import id.my.mrz.hello.spring.domain.article.event.ArticleDeletedEvent;
+import id.my.mrz.hello.spring.domain.article.repository.IArticleIndexRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ArticleDeletedEventListener implements ApplicationListener<ArticleDeletedEvent> {
+  private final IArticleIndexRepository repository;
+
+  public ArticleDeletedEventListener(IArticleIndexRepository repository) {
+    this.repository = repository;
+  }
 
   @Override
   public void onApplicationEvent(ArticleDeletedEvent event) {
-    System.out.println("EVENT: " + event.getArticleId() + " deleted");
+    long articleId = event.getArticleId();
+    repository.deleteById(articleId);
   }
 }
