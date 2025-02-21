7ab45c3b608aa445cb54f9df9067ee8d8723bf38
Mirza Hilmi <dev@mrz.my.id>
2025-02-21 13:29:32 +0700

    feat(article): index data upon article created event

--- src/main/java/id/my/mrz/hello/spring/domain/article/listener/ArticleCreatedEventListener.java
+++ src/main/java/id/my/mrz/hello/spring/domain/article/listener/ArticleCreatedEventListener.java
@@ -1,14 +1,22 @@
 package id.my.mrz.hello.spring.domain.article.listener;
 
+import id.my.mrz.hello.spring.domain.article.entity.ArticleDocument;
 import id.my.mrz.hello.spring.domain.article.event.ArticleCreatedEvent;
+import id.my.mrz.hello.spring.domain.article.repository.IArticleIndexRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ArticleCreatedEventListener implements ApplicationListener<ArticleCreatedEvent> {
+  private final IArticleIndexRepository repository;
+
+  public ArticleCreatedEventListener(IArticleIndexRepository repository) {
+    this.repository = repository;
+  }
 
   @Override
   public void onApplicationEvent(ArticleCreatedEvent event) {
-    System.out.println("EVENT: " + event.getArticle().getTitle() + " created");
+    ArticleDocument article = event.getArticle().toArticleDocument();
+    repository.save(article);
   }
 }
