00da3f16c94c413026510126f99758e5194968a2
Mirza Hilmi <dev@mrz.my.id>
2025-02-21 13:29:47 +0700

    feat(article): update index data upon article updated event

--- src/main/java/id/my/mrz/hello/spring/domain/article/listener/ArticleUpdatedEventListener.java
+++ src/main/java/id/my/mrz/hello/spring/domain/article/listener/ArticleUpdatedEventListener.java
@@ -1,14 +1,33 @@
 package id.my.mrz.hello.spring.domain.article.listener;
 
+import id.my.mrz.hello.spring.domain.article.entity.Article;
+import id.my.mrz.hello.spring.domain.article.entity.ArticleDocument;
 import id.my.mrz.hello.spring.domain.article.event.ArticleUpdatedEvent;
+import id.my.mrz.hello.spring.domain.article.repository.IArticleIndexRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ArticleUpdatedEventListener implements ApplicationListener<ArticleUpdatedEvent> {
+  private final IArticleIndexRepository repository;
+
+  public ArticleUpdatedEventListener(IArticleIndexRepository repository) {
+    this.repository = repository;
+  }
 
   @Override
   public void onApplicationEvent(ArticleUpdatedEvent event) {
-    System.out.println("EVENT: " + event.getArticle().getTitle() + " updated");
+    Article article = event.getArticle();
+
+    // FIXME: find way to handle this, retryable?
+    ArticleDocument indexed = repository.findById(article.getId()).orElseThrow();
+    ArticleDocument document = event.getArticle().toArticleDocument();
+
+    indexed.setTitle(document.getTitle());
+    indexed.setSlug(document.getSlug());
+    indexed.setContent(document.getContent());
+    indexed.setTags(document.getTags());
+
+    repository.save(indexed);
   }
 }
