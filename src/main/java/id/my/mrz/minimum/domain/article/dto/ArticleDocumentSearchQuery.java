1c4945eb6d3b9107d2cc31754d683f6ee1f5296a
Mirza Hilmi <dev@mrz.my.id>
2025-02-24 11:22:13 +0700

    feat: define artcile search query string key

--- src/main/java/id/my/mrz/minimum/domain/article/dto/ArticleDocumentSearchQuery.java
+++ src/main/java/id/my/mrz/minimum/domain/article/dto/ArticleDocumentSearchQuery.java
@@ -4,6 +4,7 @@ import id.my.mrz.minimum.domain.tag.dto.TagDocumentSearchQuery;
 import java.io.Serializable;
 import java.util.List;
 
-public final record ArticleDocumentSearchQuery(
-    String title, String slug, String content, List<TagDocumentSearchQuery> tags)
-    implements Serializable {}
+public final record ArticleDocumentSearchQuery(String query, List<TagDocumentSearchQuery> tags)
+    implements Serializable {
+  public static final String QUERY_STRING_KEY = "query";
+}
