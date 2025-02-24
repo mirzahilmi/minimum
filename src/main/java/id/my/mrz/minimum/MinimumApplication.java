a5d4ea4e6ce44dc28ce74fbc0ec6be1f6e90a6f8
Mirza Hilmi <dev@mrz.my.id>
2025-02-24 21:49:47 +0700

    chore: move api information to its spring managed bean as per documentation suggestion

--- src/main/java/id/my/mrz/minimum/MinimumApplication.java
+++ src/main/java/id/my/mrz/minimum/MinimumApplication.java
@@ -1,32 +1,11 @@
 package id.my.mrz.minimum;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-@OpenAPIDefinition(
-    info =
-        @Info(
-            title = "Minimum API",
-            description = "Minimum OpenAPI v3.1.0 Document",
-            contact = @Contact(name = "Mirza", url = "https://github.com/mirzahilmi")),
-    servers = {@Server(url = "http://localhost:8080", description = "Local Environment")})
-@SecurityScheme(
-    name = "bearerAuth",
-    description = "JWT Authentication",
-    scheme = "bearer",
-    type = SecuritySchemeType.HTTP,
-    bearerFormat = "JWT",
-    in = SecuritySchemeIn.HEADER)
 public class MinimumApplication {
   public static void main(String[] args) {
     SpringApplication.run(MinimumApplication.class, args);
---
+++ src/main/java/id/my/mrz/minimum/config/Springdoc.java
@@ -0,0 +1,27 @@
+package id.my.mrz.minimum.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.stereotype.Component;
+
+@Component
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "Minimum API",
+            description = "Minimum OpenAPI v3.1.0 Document",
+            contact = @Contact(name = "GitHub", url = "https://github.com/mirzahilmi")),
+    servers = {@Server(url = "http://localhost:8080", description = "Local Environment")})
+@SecurityScheme(
+    name = "bearerAuth",
+    description = "JWT Authentication",
+    scheme = "bearer",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    in = SecuritySchemeIn.HEADER)
+public class Springdoc {}
