7a95b5867edbe2adadc6c392a170b6fb00695fc6
Mirza Hilmi <dev@mrz.my.id>
2025-02-24 21:27:11 +0700

    chore: add openapi server url

--- src/main/java/id/my/mrz/minimum/MinimumApplication.java
+++ src/main/java/id/my/mrz/minimum/MinimumApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -16,7 +17,8 @@ import org.springframework.cache.annotation.EnableCaching;
         @Info(
             title = "Minimum API",
             description = "Minimum OpenAPI v3.1.0 Document",
-            contact = @Contact(name = "Mirza", url = "https://github.com/mirzahilmi")))
+            contact = @Contact(name = "Mirza", url = "https://github.com/mirzahilmi")),
+    servers = {@Server(url = "http://localhost:8080", description = "Local Environment")})
 @SecurityScheme(
     name = "Bearer Authentication",
     type = SecuritySchemeType.HTTP,
