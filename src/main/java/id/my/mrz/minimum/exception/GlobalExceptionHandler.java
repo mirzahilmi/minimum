2e566a8c4a480e754b04d71487559e08f5929ce5
Mirza Hilmi <dev@mrz.my.id>
2025-02-24 21:52:39 +0700

    chore: add @ResponseStatus for @ControllerAdvice

--- src/main/java/id/my/mrz/minimum/exception/GlobalExceptionHandler.java
+++ src/main/java/id/my/mrz/minimum/exception/GlobalExceptionHandler.java
@@ -4,16 +4,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public final class GlobalExceptionHandler {
   @ExceptionHandler(UsernameNotFoundException.class)
+  @ResponseStatus(code = HttpStatus.NOT_FOUND)
   public ProblemDetail handleUsernameNotFoundException(UsernameNotFoundException e) {
     return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
   }
 
   @ExceptionHandler(ResourceViolationException.class)
+  @ResponseStatus(code = HttpStatus.BAD_REQUEST)
   public ProblemDetail handleResourceViolationException(ResourceViolationException e) {
     // HACK: maybe better off put this in constants
     if (e.getTags().contains("duplicate"))
@@ -26,6 +29,7 @@ public final class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(Exception.class)
+  @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
   public ProblemDetail fallback(Exception e) {
     e.printStackTrace();
     return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
