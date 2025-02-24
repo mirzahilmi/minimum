ff1efeb2d97b6ed5a04f25e99f9457b13d3b7d1c
Mirza Hilmi <dev@mrz.my.id>
2025-02-24 15:11:01 +0700

    chore: add document info swagger section

--- src/main/java/id/my/mrz/minimum/MinimumApplication.java
+++ src/main/java/id/my/mrz/minimum/MinimumApplication.java
@@ -1,11 +1,15 @@
 package id.my.mrz.minimum;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@OpenAPIDefinition(
+    info = @Info(title = "Minimum API", description = "Minimum OpenAPI v3.1.0 Document"))
 public class MinimumApplication {
   public static void main(String[] args) {
     SpringApplication.run(MinimumApplication.class, args);
