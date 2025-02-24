53b98116a2f3a69323231ef70af0d94819e6b779
Mirza Hilmi <dev@mrz.my.id>
2025-02-24 20:31:05 +0700

    feat: add unique constraint for username

--- src/main/java/id/my/mrz/minimum/domain/user/entity/User.java
+++ src/main/java/id/my/mrz/minimum/domain/user/entity/User.java
@@ -1,6 +1,7 @@
 package id.my.mrz.minimum.domain.user.entity;
 
 import id.my.mrz.minimum.domain.user.dto.UserResourceResponse;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,7 +20,9 @@ public final class User implements UserDetails {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
+  @Column(unique = true)
   private String username;
+
   private String password;
 
   protected User() {}
